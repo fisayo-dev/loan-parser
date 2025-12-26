6e4a44c51f017858e11d4c83d02a97cc40b5284a
fisayo-dev <olufisayobadina@gmail.com>
2025-12-26 21:19:26 +0100

    style: add littpel ui responsiveness

--- web/components/page/Landing.tsx
+++ web/components/page/Landing.tsx
@@ -3,17 +3,17 @@ import Button from "../field/Button";
 const Landing = () => {
   return (
     <div className="">
-      <div className="grid place-items-center h-full">
+      <div className="grid place-items-center h-auto lg:h-full">
         <div className="2xl:py-20 py-10 flex flex-col items-center justify-center text-center">
           <div className="flex flex-col">
-            <h2 className="leading-20 text-4xl lg:text-7xl font-bold">
+            <h2 className="lg:leading-20 text-4xl lg:text-7xl font-bold">
               Simplify Complex <br></br> Loan Documents
             </h2>
             <p className="text-sm md:text-2xl mt-6 mb-10">
               AI-powered loan summary
             </p>
           </div>
-          <Button text="Start for free" className="text-2xl px-10" />
+          <Button text="Start for free" className="lg:text-2xl px-10" />
         </div>
       </div>
     </div>
