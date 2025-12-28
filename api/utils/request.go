5c9064571c541031158265d046ce53a327442aa3
fisayo-dev <olufisayobadina@gmail.com>
2025-12-28 19:19:48 +0100

    chore: add comment

--- api/utils/request.go
+++ api/utils/request.go
@@ -9,5 +9,6 @@ func DecodeJSONRequest(r *http.Request, dst interface{}) error {
 	decoder := json.NewDecoder(r.Body)
 	decoder.DisallowUnknownFields()
 
+	// decoder.Decode() decoes the request params into the dst struct
 	return decoder.Decode(dst)
 }
