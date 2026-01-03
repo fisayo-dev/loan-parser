ae287e532b7000d9599b2618b6218595f24d6d82
fisayo-dev <olufisayobadina@gmail.com>
2026-01-03 21:49:35 +0100

    style: re-style scan page

--- web/components/page/Header.tsx
+++ web/components/page/Header.tsx
@@ -1,12 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
 import { Star } from "lucide-react";
 import Button from "../field/Button";
-import Link from "next/link";
 
 const Header = () => {
   return (
     <div className="z-50 fixed top-0 w-full grid items-center border-b border-border h-20 bg-background/80 backdrop-blur-md">
       <div className="app-container w-full flex justify-between items-center">
-        <h2 className="text-2xl font-bold">Loan Parser</h2>
+        <Link href="/" className="flex items-center gap-2">
+            <Image src="/logo.png" alt="Loan Parser Logo" width={32} height={32} className="w-8 h-8" />
+            <h2 className="text-xl font-bold tracking-tight">Loan Parser</h2>
+        </Link>
 
         <div className="flex items-center space-x-2">
           <Link
